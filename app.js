de592633c4fc4447327865d7e7feb18b02540c8b
himanish <himanish.kaushal@gmail.com>
2020-01-19 17:41:06 -0500

    OTLT-1 - Add API endpoints
    - Add mock data
    - Add endpoints and methods to serve GET requests for resource socket

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+let sockets = [
+    { id: 1, status: 'on', location: 'US' },
+    { id: 2, status: 'on', location: 'Canada' },
+    { id: 3, status: 'off', location: 'Puerto Rico' },
+    { id: 4, status: 'on', location: 'Canada' },
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Welcome to the Swidget world!');
+});
+
+app.get('/sockets', (req, res) => {
+    res.send(sockets);
+});
+
+app.get('/sockets/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(sockets.filter(v => v.id == id));
 });
 
 app.listen(PORT);
